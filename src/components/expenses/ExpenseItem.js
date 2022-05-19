c8129e1f9e937c216ae599e394bf79d1b1826991
Sergio Terrero <sergio-terrero@hotmail.com>
2022-05-19 00:32:12 -0400

    useState section completed

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,17 +1,38 @@
 import ExpenseDate from "./ExpenseDate.js";
+import React, { useState } from 'react'
+
 import "./ExpenseItem.css";
 import Card from "../UI/Card.js";
-const ExpenseItem = (props)=> {
+
+
+const ExpenseItem = (props) => {
+  /* useState is a hook to make state changes possible
+   * title: name of old variable.
+   * setTitle: name of new title variable. conventinal name used to make the new state of the event change,click etc..
+   *[title,setTitle] return an array with old and new values.
+   * const does not affect constructor because doesnot have the = sign 
+  */
+
+  const [title, setTitle] = useState(props.title); //? useState always returns an array, with thos two elements, old and new state.
+  console.log('expense item ')
+  
+  const clickHandler = () => {
+  
+    setTitle('updated');
+    console.log(title);
+
+  } //? convention used to call event click,hover ending in Handler
 
   return (
     <Card className="expense-item">
-   <ExpenseDate date={props.date}/>
-    
+      <ExpenseDate date={props.date} />
+
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-      
-      <div className="expense-item__price" >{props.amount}</div>
+        <h2>{title}</h2>
+
+        <div className="expense-item__price" >{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
